59fa7f9f9a8fa9a1df34ad60fb01b5339a0a7616
umar <jUmarB@protonmail.com>
2023-09-21 10:06:17 +0000

    basic arguments and program  basename without  ./

--- app/main.c
+++ app/main.c
@@ -13,19 +13,70 @@
 #include <dirent.h>  
 #include <errno.h> 
 #include <err.h> 
-#include <stdio.h> 
-
-#define LKMD_TAG  "LKMD" 
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h> 
 
 
 #include "lkmd.h" 
+
+#define VERSION  "1.0"
+
+#define BASENAME(ac , av) ({\
+    char * bn_elfbin= av[ac^1] ;\
+    char  bn[0x14] = {0} ; \
+    memcpy( bn, (bn_elfbin+2) , 0x14);\
+    printf("%s\n",bn);})
+
+#define basename(ac, av) BASENAME(ac,av) 
+
+
+
+#define LKMD_TAG  "LKMD" 
+
+void  arguments_parser ( int  argcount , char * const  * argvalues ) 
+{
+
+  printf("%i\n",   argcount)  ; 
+  if ( argcount   <=1  )
+  {     //display usage 
+    
+     basename(argcount , argvalues) ; 
+     exit(1) ; 
+  }
+  int option=0 ; 
+  const char *short_flags = ":vh" ;
+  while ( (option = getopt(argcount , argvalues , short_flags)) !=  ~0  ) 
+  {
+    switch ( option ) 
+    {
+        case 'v' : 
+          fprintf(stdout , "%s\n", VERSION) ;
+          exit(0) ; 
+          break; 
+        case 'h' : 
+          puts("should display usage of the programme") ; 
+          exit(0) ; 
+          break ;
+          
+        default : 
+          puts("unknow flag options") ; 
+          exit(0); 
+          break; 
+     }
+   }
+   
+}
+
 
 
 
 int
 main (int __ac  , char ** __av) 
 {
- 
+
+  arguments_parser(__ac, __av);   
+
 
   lkmd_t  module_list ;   
   lkmd_syspath_open(_nullable, &module_list)  ; 
